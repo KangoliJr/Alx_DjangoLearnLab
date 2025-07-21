d7a73bf47e2662bad1dc94a1d1d5bf5178cabd00
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-07-22 01:56:02 +0300

    swapped to fbv

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -16,12 +16,22 @@ class LibraryDetailView(DetailView):
     template_name = "relationship_app/library_detail.html"
     context_object_name = 'library'
     
+# def register(request):
+#     return RegisterView.as_view(template_name='relationship_app/register.html')(request)
+# class RegisterView(CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('register')
+#  also correct 
+# using function based views
 def register(request):
-    return RegisterView.as_view(template_name='relationship_app/register.html')(request)
-class RegisterView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('register')
-
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse_lazy('login'))
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
 class LoginView(LoginView):
     success_url = reverse_lazy('login')
 
