bde44c97139b282a4fc89905ae82f1dd54227d90
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-07-22 00:54:46 +0300

    added template_name

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.RegisterView.as_view(), name='register'),
-    path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='register'),
+    path('register/', views.RegisterView.as_view(template_name = 'relationship_app/register.html'), name='register'),
+    path('login/', views.CustomLoginView.as_view(template_name = 'relationship_app/login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(template_name = 'relationship_app/logout.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -19,12 +19,9 @@ class LibraryDetailView(DetailView):
 class RegisterView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('register')
-    template_name = 'relationship_app/register.html'
 
 class CustomLoginView(LoginView):
-    template_name = 'relationship_app/login.html'
     success_url = reverse_lazy('login')
 
 class CustomLogoutView(LogoutView):
-    template_name = 'relationship_app/logout.html'
     success_url = reverse_lazy('logout')    
