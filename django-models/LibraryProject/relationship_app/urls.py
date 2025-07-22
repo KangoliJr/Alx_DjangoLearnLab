c2253f710ff279110a639a0ca553be6adfc5dc30
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-07-22 23:24:31 +0300

    corrected urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path('admin/', views.admin_view, name='admin-view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member-view'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    path('books/add/', views.add_book, name='add_book/'),
+    path('books/<int:pk>/edit/', views.edit_book, name='edit_book/'),
+    path('books/<int:pk>/delete/', views.delete_book, name='delete_book/'),
 
 ]
