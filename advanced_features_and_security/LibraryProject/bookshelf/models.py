3d2143b0e246b4eed19cca4ee3cca1c6f4a69bc1
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-07-27 16:09:21 +0300

    half way part 1

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Book(models.Model):
@@ -8,3 +9,8 @@ class Book(models.Model):
     
 def __str__(self):
     return f"{self.title} by {self.author}, ({self.publication_year})"
+
+
+class User(AbstractUser):
+    date_of_birth = models.DateField
+    profile_photo = models.ImageField
