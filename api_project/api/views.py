a36f2fbca71f58b87ba7dffffcc02590fcd4e6a7
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-08-03 23:03:25 +0300

    task 3

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import permissions
 # Create your views here.
 # task 1
 # class BookList(generics.ListAPIView):
@@ -13,3 +14,4 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework.authtoken import views
+path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
