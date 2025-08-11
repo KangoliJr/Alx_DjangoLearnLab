b349cffc6483c9596efd1fed0078c88d4c51cc58
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-08-11 14:31:14 +0300

    task 2

--- advanced-api-project/README.md
+++ advanced-api-project/README.md
@@ -11,3 +11,7 @@ methods
 GET
 PUT
 DELETE
+
+
+filtering
+GET/api/books/
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,12 +3,20 @@ from rest_framework import generics, permissions
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']
+
+        
     
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
@@ -31,6 +39,6 @@ class BookUpdateView(generics.UpdateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     
 class BookDeleteView(generics.DestroyAPIView):
-    queryset = Book.objescts.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
