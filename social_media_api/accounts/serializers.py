1cc8e686f7463a1583fe84d0eec6fac93f57eeed
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-08-29 12:01:18 +0300

    corrected the serializers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from .models import User
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    email = serializers.CharField(required=True)
     password = serializers.CharField(write_only= True)
     confirm_password = serializers.CharField(write_only= True)
-    
+    bio = serializers.CharField(required=False, allow_blank=True)
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'confirm_password']
@@ -19,15 +21,22 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({"password":"password does not match"})
         return data
     def create(self,validated_data):
-        validated_data.pop('confirm_password')
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            bio=validated_data.get('bio', '')
+        username=validated_data['username'],
+        email=validated_data['email'],
+        password=validated_data['password'],
+        bio=validated_data.get('bio', '')
+        
+        user = get_user_model().objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            bio=bio
         )
+        
+        Token.objects.create(user=user)
         return user
-    
+
+
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
