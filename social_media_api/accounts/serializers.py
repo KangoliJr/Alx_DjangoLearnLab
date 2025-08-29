f2e5ad13f3390f0ddf1f97bd5ef455a86c51bef7
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-08-29 11:54:30 +0300

    task 0

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from .models import User
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only= True)
+    confirm_password = serializers.CharField(write_only= True)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'confirm_password']
+        extra_kwargs = {
+            'email':{'required':True},
+        }
+        
+    def validate(self, data):
+        if data['password'] !=data['confirm_password']:
+            raise serializers.ValidationError({"password":"password does not match"})
+        return data
+    def create(self,validated_data):
+        validated_data.pop('confirm_password')
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', '')
+        )
+        return user
+    
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            token, created = Token.objects.get_or_create(user=user)
+            data['token'] = token.key
+            return data
+        raise serializers.ValidationError("Invalid Credentials")
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from rest_framework import generics, status
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+# Create your views here.
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            "message": "User registered successfully.",
+            "token": token.key
+        }, status=status.HTTP_201_CREATED)
+
+class UserLoginView(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
