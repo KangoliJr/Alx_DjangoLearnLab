c574a3c7b21696a44fc97f816350b39899cb71fc
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-08-30 03:34:06 +0300

    added x frames

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -146,6 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 # Enable XSS protection
 SECURE_BROWSER_XSS_FILTER = True 
+X_FRAME_OPTIONS = True
 
 # Prevent MIME type sniffing
 SECURE_CONTENT_TYPE_NOSNIFF = True 
