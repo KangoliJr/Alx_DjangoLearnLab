609370e0f3042a2f3117c5e19cf2d8295280d479
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-09-15 20:43:46 +0300

    added the correct library project

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%)n+959f)!zql-2qq78ijzjd60i-d^ehq4w04#m9^@mcqfolhk'
+SECRET_KEY = 'django-insecure-jt#oz(x54q_1vd^^(-&fcxatfuxx42twij4rlxsab19^*9udv@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "bookshelf.apps.BookshelfConfig"
+    "bookshelf.apps.BookshelfConfig",
+    "relationship_app.apps.RelationshipAppConfig"
 ]
 
 MIDDLEWARE = [
