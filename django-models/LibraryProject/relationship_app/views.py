016153d895b4a5509b815e3ed0bfd8fc45764474
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-09-15 21:35:19 +0300

    added relationship

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import Library
 # Create your views here.
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
